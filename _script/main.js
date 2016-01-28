d4279132b62bf3597ae143995524a930f7e0e7bf
l4nk332 <l4nk332@gmail.com>
2016-01-28 13:24:24 -0800

    Fix the draggable jquery ui feature so that first element can be prepended. Add slight css color transition for buttons on hover state. Imported the jquery ui css stylesheet for further ui use.

--- _script/main.js
+++ _script/main.js
@@ -1,59 +1,63 @@
-// If text field is empty then checkbox is disabled and
-// blurred
-$(".text").blur(function() {
-  if(!this.value) {
-    $(this).prev(".check").addClass("strikeToggle");
-    $(this).prev(".check").prop("disabled", true);
-  } else {
-    $(this).prev(".check").removeClass("strikeToggle");
-    $(this).prev(".check").prop("disabled", false);
-  }
-});
-// Toggle strikethrough item and move it to the top
-// when checked is true.
-$(".check").on('click', function() {
-$(this).next('.text').toggleClass("strikeToggle");
-if($(this).next(".text").prop("disabled")) {
-    $(this).next('.text').prop("disabled", false);
-  } else {
-    $(this).next('.text').prop("disabled", true);
-  }
-});
+$(document).ready(function() {
 
-// Toggle Show/Hide Completed
-$("#hiddenButton").click(function() {
-  $("ul").children("li").each(function () {
-    if ($(this).children(".check").prop("checked")) {
-      $(this).toggleClass("hiddenToggle");
+  // If text field is empty then checkbox is disabled and
+  // blurred
+  $(".text").blur(function() {
+    if(!this.value) {
+      $(this).prev(".check").addClass("strikeToggle");
+      $(this).prev(".check").prop("disabled", true);
+    } else {
+      $(this).prev(".check").removeClass("strikeToggle");
+      $(this).prev(".check").prop("disabled", false);
     }
   });
-});
+  // Toggle strikethrough item and move it to the top
+  // when checked is true.
+  $(".check").on('click', function() {
+  $(this).next('.text').toggleClass("strikeToggle");
+  if($(this).next(".text").prop("disabled")) {
+      $(this).next('.text').prop("disabled", false);
+    } else {
+      $(this).next('.text').prop("disabled", true);
+    }
+  });
+
+  // Toggle Show/Hide Completed
+  $("#hiddenButton").click(function() {
+    $("ul").children("li").each(function () {
+      if ($(this).children(".check").prop("checked")) {
+        $(this).toggleClass("hiddenToggle");
+      }
+    });
+  });
 
 
-// On hover show 'X'
-$(".list-item").hover(
-  function() {
-    $(this).children(".deleteIcon").removeClass("hiddenToggle");
-  },
-  function() {
-    $(this).children(".deleteIcon").addClass("hiddenToggle");
-  }
-);
+  // On hover show 'X'
+  $(".list-item").hover(
+    function() {
+      $(this).children(".deleteIcon").removeClass("hiddenToggle");
+    },
+    function() {
+      $(this).children(".deleteIcon").addClass("hiddenToggle");
+    }
+  );
 
 
-// 'X'.onclick remove item
-// and delete the node element
-$(".deleteIcon").click( function() {
-  $(this).parent("li").fadeOut(300, function() { $(this).remove(); });
-});
+  // 'X'.onclick remove item
+  // and delete the node element
+  $(".deleteIcon").click( function() {
+    $(this).parent("li").fadeOut(300, function() { $(this).remove(); });
+  });
 
-// Create a way to add list items
-// appending them to the end using templating
+  // Create a way to add list items
+  // appending them to the end using templating
 
 
 
-// Allow list-item to be dragged and moved
-$("ul").sortable({
-  cursor: "move",
-  containment: "ul"
+  // Allow list-item to be dragged and moved
+  $("ul").sortable({
+    cursor: "move",
+    containment: "#list-container"
+  });
+  
 });
