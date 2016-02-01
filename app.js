52974d83176fd666f3d805314d1047a2c813f344
l4nk332 <l4nk332@gmail.com>
2016-01-31 20:27:09 -0800

    Adds a custom middle-ware function that logs request methods and the url paths being requested by the client

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require("express");
 
 var app = express();
 
+
+app.use(function(req, res, next) {
+
+  console.log(req.method + " request for " + req.url);
+  next();
+});
+
 app.use(express.static("./public"));
 
 
